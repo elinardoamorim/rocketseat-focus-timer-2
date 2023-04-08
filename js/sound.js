fe83e1a572e9e28761fa59c6bdec57e0258b6536
Elinardo Amorim <elinardoslva@gmail.com>
2023-04-07 21:37:39 -0300

    fix song paths

--- js/sound.js
+++ js/sound.js
@@ -1,8 +1,8 @@
 export default function () {
-  const songForest = new Audio("../sounds/forest.wav")
-  const songRain = new Audio("../sounds/rain.wav")
-  const songCoffeeShop = new Audio("../sounds/coffeeshop.wav")
-  const songFirePlace = new Audio("../sounds/fireplace.wav")
+  const songForest = new Audio("https://github.com/elinardoamorim/rocketseat-focus-timer-2/blob/master/sounds/forest.wav?raw=true")
+  const songRain = new Audio("https://github.com/elinardoamorim/rocketseat-focus-timer-2/blob/master/sounds/rain.wav?raw=true")
+  const songCoffeeShop = new Audio("https://github.com/elinardoamorim/rocketseat-focus-timer-2/blob/master/sounds/fireplace.wav?raw=true")
+  const songFirePlace = new Audio("https://github.com/elinardoamorim/rocketseat-focus-timer-2/blob/master/sounds/coffeeshop.wav?raw=true")
 
   return {
     songForest,
